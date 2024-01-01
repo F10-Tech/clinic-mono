e3292d550b53b7fa4533ee4c70eca9983a6dc32a
itsBadr28 <badreddine.ad@univ-constantine2.dz>
2024-01-01 21:22:58 +0100

    add the order

--- clinic-api-master/order/serializers.py
+++ clinic-api-master/order/serializers.py
@@ -1,74 +1,27 @@
 from rest_framework import serializers
 from .models import *
-from core.models import User
-from django.db import transaction
-from service.models import *
+import datetime
+
 
 class OrderSerializer(serializers.ModelSerializer):
 
+    patient = serializers.SerializerMethodField('get_patient_data')
 
-    customer = serializers.SerializerMethodField('get_customer_data')
-    def get_customer_data(self, obj):
-        data = {
-            'first_name': obj.customer.first_name,
-            'last_name': obj.customer.last_name,
-            'phone': obj.customer.phone
-        }
-        return data
-    
-    subservice = serializers.SerializerMethodField('get_subservice_data')
-    service = serializers.SerializerMethodField('get_service_data')
-
-    def get_service_data(self, obj):
+    def get_patient_data(self, obj):
         
-        data = {
-            'image_light': obj.subservice.first().service.image_light.name,
-            'image_dark': obj.subservice.first().service.image_dark.name,
-            'name_en': obj.subservice.first().service.name_EN,
-            'name_ar': obj.subservice.first().service.name_AR,
-            'name_fr': obj.subservice.first().service.name_FR
+        date = {
+            'id': obj.patient.id,
+            'name': obj.patient.name
         }
-        return data
-
-
+        return date
     
-    def get_subservice_data(self, obj):
-        data = []
-        for i in obj.subservice.all():
-            names = {
-                'name_en': i.name_EN,
-                'name_ar': i.name_AR,
-                'name_fr': i.name_FR,
-                'id': i.id
-            }
-            data.append(names)
-        return data
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Order
-        fields = ['id','customer','subservice','scheduled','location','price','state', 'service','created_at']
+        fields = '__all__'
 
 class CreateOrderSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)
+
     class Meta:
         model = Order
-        fields = ['subservice','scheduled','location','price', 'id']
-
-    def save(self, **kwargs):
-        with transaction.atomic():
-            (customer_id, create) = User.objects.only('id').get_or_create(id=self.context['user_id'])
-            order_create = Order.objects.create(customer = customer_id)
-            order = Order.objects.filter(id = order_create.id).first()
-            self.validated_data['id'] = order_create.id
-            sub = self.validated_data['subservice']
-            for i in sub:
-                order.subservice.add(i)
-            order.scheduled = self.validated_data['scheduled']
-            order.location = self.validated_data['location']
-            order.price = self.validated_data['price']
-            order.save()
-
-class UpdateOrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['state']
+        fields = '__all__'
--- clinic-api-master/order/views.py
+++ clinic-api-master/order/views.py
@@ -2,71 +2,49 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response 
-from core.models import User
 from .serializers import *
 from .models import Order
-from .operations import Change, Get
+from datetime import datetime
+from rest_framework import status
 
 
 @api_view(['GET'])
-def OneOrder(request, pk):
-    return Get(request, OrderSerializer, Order.objects.get(id = pk), pk, False)
+def OrdersList(request, pk):
+    orders = Order.objects.filter(created_at__date=pk)
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
-def ChangeStateToPending(request,pk):
-    return Change(Order.objects.get(id = pk), OrderSerializer, pk, 'PENDING')
-
-@api_view(['GET'])
-def ChangeStateToInProgress(request,pk):
-    if request.user.is_staff:
-        return Change(Order.objects.get(id = pk), OrderSerializer, pk, 'INPROGRESS')
+def Total(request, pk):
+    if pk == 'month':
+        orders = Order.objects.filter(created_at__month=datetime.today().month)
+        total = 0
+        for order in orders:
+            total = total + order.total
+        return Response(total)
+    elif pk == 'year':
+        orders = Order.objects.filter(created_at__year=datetime.today().year)
+        total = 0
+        for order in orders:
+            total = total + order.total
+        return Response(total)
     else:
-        return Response('No permission')
-
-@api_view(['GET'])
-def ChangeStateToCompleted(request,pk):
-    if request.user.is_staff:
-        return Change(Order.objects.get(id = pk), OrderSerializer, pk, 'COMPLETED')
-    else:
-        return Response('No permission')
-
-@api_view(['GET'])
-def ChangeStateToReject(request,pk):
-    return Change(Order.objects.get(id = pk), OrderSerializer, pk, 'REJECTED')
-
-@api_view(['GET'])
-def ChangeStateToCanceled(request,pk):
-    return Change(Order.objects.get(id = pk), OrderSerializer, pk, 'CANCELLED')
-
-@api_view(['GET'])
-def ListOfOrderByCustomer(request,pk):
-    return Get(request, OrderSerializer, Order.objects.filter(customer = pk), pk, True)
-
-class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return CreateOrderSerializer
-        elif self.request.method == 'PATCH':
-            return UpdateOrderSerializer
-        return OrderSerializer
+        orders = Order.objects.filter(created_at__date=pk)
+        total = 0
+        for order in orders:
+            total = total + order.total
+        return Response(total)
     
-    def list(self, request):
-        if request.user.is_staff:
-            queryset = Order.objects.all()
-        else:
-            queryset = Order.objects.filter(customer = request.user.id)
-        serializer = OrderSerializer(queryset, many=True)
-        return Response(serializer.data)
 
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
+@api_view(['POST'])
+def CreateOrder(request):
+    serializer = CreateOrderSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        patient = Patient.objects.get(id=request.data['patient'])
+        patient.rest = patient.rest - request.data['total']
+        patient.save()
+        serializer.save()
+    return Response(serializer.data)
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_staff:
-            return Order.objects.all()
         
-        (customer_id, create) = User.objects.only('id').get_or_create(id = user.id)
-        return Order.objects.filter(customer = customer_id)   
